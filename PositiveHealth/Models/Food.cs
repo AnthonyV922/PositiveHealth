eb66fff8e762a37a1e665f30b17f2d035589cc2b
AnthonyV922 <anthony_922@hotmail.com>
2018-05-30 02:16:05 -0400

    Add files via upload

---
+++ PositiveHealth/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PositiveHealth.Model
+{
+    public class Food
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Calories { get; set; }
+
+    }
+}
